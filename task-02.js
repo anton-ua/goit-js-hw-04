d144d972c2eb7f9cd9529c9c694934fa7a507695
anton-ua <antonyua23@gmail.com>
2019-12-01 14:33:24 +0200

    hw-04

--- task-02.js
+++ task-02.js
@@ -1,15 +1,17 @@
+const add = function(itemName) {
+  console.log(`Adding ${itemName} to inventory`);
+
+  this.items.push(itemName);
+};
+
+const remove = function(itemName) {
+  console.log(`Removing ${itemName} from inventory`);
+
+  this.items = this.items.filter(item => item !== itemName);
+};
+
 const inventory = {
-  items: ["Knife", "Gas mask"],
-  add(itemName) {
-    console.log(`Adding ${itemName} to inventory`);
-
-    this.items.push(itemName);
-  },
-  remove(itemName) {
-    console.log(`Removing ${itemName} from inventory`);
-
-    this.items = this.items.filter(item => item !== itemName);
-  }
+  items: ["Knife", "Gas mask"]
 };
 
 const invokeInventoryAction = function(itemName, action) {
@@ -17,13 +19,13 @@ const invokeInventoryAction = function(itemName, action) {
   action(itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add);
+invokeInventoryAction("Medkit", add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction("Gas mask", inventory.remove);
+invokeInventoryAction("Gas mask", remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
